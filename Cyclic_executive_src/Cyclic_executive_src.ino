f8e4df31d1b41fabf2da5af50e6036f4843d9e63
Spike Wilson <113636901+SpikeWils@users.noreply.github.com>
2023-03-15 20:24:54 +0000

    All functions included in src
    All complete functions included in source code

--- Cyclic_executive_src/Cyclic_executive_src.ino
+++ Cyclic_executive_src/Cyclic_executive_src.ino
@@ -4,8 +4,8 @@ NOTES
 Task 1 = Output signal
 Task 2 = Measure square wave frequency 333-1000Hz
 Task 3 = Measure square wave frequency 500-1000Hz
-Task 4 = 
-Task 5 =
+Task 4 = Read average analogue value & set LED
+Task 5 = Print frequency of task 2 & 3 and map it to 0-99
 
 */
 
@@ -19,6 +19,8 @@ Task 5 =
 #define SIGNAL_OUTPUT_PIN 17
 #define SQUARE_WAVE_1_PIN 33
 #define SQUARE_WAVE_2_PIN 32
+#define POT_PIN 36
+#define LED_PIN 19
 #define FRAME_DURATION_MS 2  // 2ms
 
 /*****************************************************
@@ -28,8 +30,14 @@ Task 5 =
 int SignalOutputPin = SIGNAL_OUTPUT_PIN;
 int SquareWave1Pin = SQUARE_WAVE_1_PIN;
 int SquareWave2Pin = SQUARE_WAVE_2_PIN;
+int LEDpin = LED_PIN;
+int PotPin = POT_PIN;
+float frequency1 = 0.0;
+float frequency2 = 0.0;
+int frequency1mapped = 0;
+int frequency2mapped = 0;
 
-int inputPin = 13;
+
 unsigned long frameTime = 0;
 unsigned long frameCounter = 0;
 
@@ -48,6 +56,10 @@ void setup(void)
   Serial.println("Ready");
   frame();
   ticker.attach_ms(4, frame);
+
+  pinMode(PotPin, INPUT);
+  pinMode(LEDpin, OUTPUT);
+
 }
 
 /*****************************************************
@@ -62,7 +74,6 @@ void frame()
     case 0:
 
       JobTask1();
-      JobTask3();
 
       break;
 
@@ -156,31 +167,54 @@ float JobTask2(int SquareWave1Pin)
 {
   monitor.jobStarted(2);
   // measure the period~_high of the input signal using the pulseIn function
-  unsigned int period_high = pulseIn(SquareWave1Pin, HIGH);
+  unsigned int period_high1 = pulseIn(SquareWave1Pin, HIGH);
   //unsigned int period_low = pulseIn(inputPin, LOW);
 
   // calculate the frequency from the period~_high and print it to the serial monitor
-  float frequency = 1.0 / ((period_high * 2) * 0.000001);
+  frequency1 = 1.0 / ((period_high1 * 2) * 0.000001);
   
-  Serial.print(frequency);
+  Serial.print(frequency1);
 
-  return frequency;
+  return frequency1;
   monitor.jobEnded(2);
 }
 
 
-void JobTask3(void)
+float JobTask3(int SquareWave2Pin)
 {
   monitor.jobStarted(3);
-  delayMicroseconds(1000);
+  // measure the period~_high of the input signal using the pulseIn function
+  unsigned int period_high2 = pulseIn(SquareWave2Pin, HIGH);
+  //unsigned int period_low = pulseIn(inputPin, LOW);
+
+  // calculate the frequency from the period~_high and print it to the serial monitor
+  frequency2 = 1.0 / ((period_high2 * 2) * 0.000001);
+  
+  Serial.print(frequency2);
+
+  return frequency2;
   monitor.jobEnded(3);
 }
+
 
 
 void JobTask4(void)
 {
   monitor.jobStarted(4);
-  delayMicroseconds(2000);
+  int reading1 = analogRead(PotPin);
+  int reading2 = analogRead(PotPin);
+  int reading3 = analogRead(PotPin);
+  int reading4 = analogRead(PotPin);
+  int readingavg = (reading1 + reading2 + reading3 + reading4) / 4;
+
+  if (readingavg > (4095/2))
+  {
+  digitalWrite(LEDpin, HIGH);
+  }
+  else
+  {
+  digitalWrite(LEDpin, LOW);
+  }
   monitor.jobEnded(4);
 }
 
@@ -188,6 +222,13 @@ void JobTask4(void)
 void JobTask5(void)
 {
   monitor.jobStarted(5);
-  delayMicroseconds(2000);
+
+  frequency1mapped = map(frequency1, 333, 1000, 0, 99);
+  frequency2mapped = map(frequency2, 500, 1000, 0, 99);
+
+  Serial.print(frequency1mapped);
+  Serial.print(",");
+  Serial.println(frequency2mapped);
+
   monitor.jobEnded(5);
 }
